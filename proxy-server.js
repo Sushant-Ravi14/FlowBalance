a3264e3fbdcdf0bbd6825ae38102867a3d43741a
Sushant-Ravi14 <sushant.ravi.cg@gmail.com>
2026-08-30 07:15:00 +0530

    feat(proxy): initialize backend registry state management

--- proxy-server.js
+++ proxy-server.js
@@ -1,10 +1,23 @@
 const http = require('http');
 
 class ProxyServer {
+    constructor(backends, algorithm = 'round-robin') {
+        this.backends = backends.map(b => ({
+            id: `${b.host}:${b.port}`,
+            host: b.host,
+            port: b.port,
+            isHealthy: true,
+            activeConnections: 0,
+            latency: 0,
+            failures: 0
+        }));
+        this.algorithm = algorithm;
+        this.rrIndex = 0;
+    }
     start(port = 8080) {
         this.server = http.createServer((req, res) => this.handleIncomingRequest(req, res));
         this.server.listen(port, () => {
-            console.log(`[SYSTEM] FlowBalance Proxy listening on port ${port}`);
+            console.log(`[SYSTEM] FlowBalance Proxy listening on port ${port} | Algorithm: ${this.algorithm}`);
         });
     }
 
